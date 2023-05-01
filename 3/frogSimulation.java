50ee58a7725bb59fe3ac65f0b5948f2e2fa70454
Hein Thu <74117599+H4CK3RG0D@users.noreply.github.com>
2023-05-01 13:25:56 -0500

    Update frogSimulation.java

--- 3/frogSimulation.java
+++ 3/frogSimulation.java
@@ -1,1 +1,34 @@
+class frogSimulation {
+    private int goalDistance;
+    private int maxHops;
 
+    public int hopDistance() {
+        /* implemantation not shown */
+    }
+    public boolean simulate() {
+        // (a)
+        int pos = 0;
+        for (int count = 0; count < maxHops; count++) {
+            pos += hopDistance();
+
+            if (pos >= goalDistance) {
+                return true;
+            } 
+            else if {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public double runSimulations(int num) {
+        // (b)
+        int countSucc = 0;
+        for (int count = 0; count < num; count++) {
+            if (simulate()) {
+                countSucc++;
+            }
+            return (double)countSucc / num;
+        }
+    }
+}
